2d9b0facb117629b177d39973ce7ab5e489fdbb3
dhouha <dhouha.mansour11@gmail.com>
2018-12-08 12:03:14 +0100

    revision one

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+                
+                $("#about_scroll").fadeOut();   
+                $("#work_scroll").fadeOut();
+                $("#contact_scroll").fadeOut();
+
+                $(".about").click(function(){
+                    $("#index").fadeOut();
+                    $("#about_scroll").fadeIn();
+                    $('#about_left').addClass('animated slideInLeft');
+                    $('#about_right').addClass('animated slideInRight');
+                    });
+                $(".work").click(function(){
+                    $("#index").fadeOut();
+                    $("#work_scroll").fadeIn();
+                    $('#work_left').addClass('animated slideInLeft');
+                    $('#work_right').addClass('animated slideInRight');
+                    });
+                $(".contact").click(function(){
+                    $("#index").fadeOut();
+                    $("#contact_scroll").fadeIn();
+                    $('#contact_left').addClass('animated slideInLeft');
+                    $('#contact_right').addClass('animated slideInRight');
+                    });
+                
+                $(".back").click(function(){
+                    $(".pages").fadeOut();
+                    $("#index").fadeIn();
+                    $('#main-left').addClass('animated slideInLeft');
+                    $('#main-right').addClass('animated slideInRight');
+                    });
+           
+		});
